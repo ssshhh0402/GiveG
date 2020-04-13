45bb12b6e0d520b6067602bd1d1b4905c00a0dbc
정대윤 <wjdeodbs3244@naver.com>
2020-04-13 08:44:58 +0000

    Revert ":new: donation 완료 및 자료구조 변경 map => compoisiteKey로 따로 분리"
    This reverts commit 707103b2b05ed818df8aa2a152e26f2dd559bb9e

--- BlcokChain/chaincode/donation_v01.js
+++ BlcokChain/chaincode/donation_v01.js
@@ -60,7 +60,7 @@ var Chaincode = class {
 
         let duplicateCheck = await stub.getState(compositeKey);
         //get을 통해 가져와서 등록전에 똑같은애가 있는지를 확인한다.
-        let isExist = function (value) {
+        var isExist = function (value) {
             if (value == "" || value == null || value == undefined ||
                 (value != null && typeof value == "object" && !Object.keys(value).length)) {
                 return true;
@@ -73,15 +73,15 @@ var Chaincode = class {
             throw new Error('ResisterUser : UserAccount' + compositeKey + 'is already registered.');
         }
 
-        let userInfo = {
+        var userInfo = {
             userID: '',
             userPW: '',
             auth: '',
             refundAmount: '',
             totalDonationAmount: 0,
-            participatedDonationProjects: [],
-            organizedDonationProject: [],
-            createdAt: '',
+            participatedDanationProjects: [],
+            organizedDanationProject: [],
+            createdAt:'',
         }
         userInfo.userID = args[0];
         userInfo.userPW = args[1];
@@ -109,19 +109,19 @@ var Chaincode = class {
     async registerDonationProject(stub, args) {
 
         //추가할 부분 user에 해당 프로젝트를 추가해야된다.
-
+        
         if (args.length != 6) { //여기에 들어올 args 숫자를 명시해서 처리해준다. 일단 4
             throw new Error('Incorrect Number of arguments Expecting 6, but received input args.length : ' + args.length);
         }
         let compositeKey = stub.createCompositeKey("DonationProject.ID.", [args[0]]);
         //이거 "Asset." 그렇게 엄처나게 큰 의미가 있는것은아니고 ObjectType을 명시하기 위함인데
         //뒤의 arg와 합처저서 복합키를 많어 낸다.
-
+        
         let duplicateCheck = await stub.getState(compositeKey);
         //get을 통해 가져와서 등록전에 똑같은애가 있는지를 확인한다.
-
-        let isExist = function (value) {
-
+        
+        var isExist = function (value) {
+            
             if (value == "" || value == null || value == undefined ||
                 (value != null && typeof value == "object" && !Object.keys(value).length)) {
                 return true;
@@ -130,14 +130,14 @@ var Chaincode = class {
                 return false;
             }
         };
+        
         if (isExist(duplicateCheck) != true) {
             throw new Error('DonationProjectID' + compositeKey + 'is already registered.');
         }
-
-        if (args[4].length.split(',').length !== args[5].split(',').length) {
+        if(args[4].length.split(',').length!==args[5].split(',').length){
             throw new Error(`projectGoals and goalAmounts is not equals length`);
         }
-        if (args[4].length.split(',').length != 0) {
+        if(args[4].length.split(',').length!=0){
             throw new Error(`donationProject must have one or more goals`);
         }
         let donationProjectInfo = {
@@ -151,11 +151,11 @@ var Chaincode = class {
             fundraisingEndDate: '',
             state: ''
         };
-
+        
         projectGoalMap = new Map();
         const goalAmounts = args[5].split(',');
-        args[4].split(',').forEach((item, index) => {
-            projectGoalMap.set(item, { 'amount': goalAmounts[index], 'compelete': false, 'contents': null });
+        args[4].split(',').forEach((item,index)=>{
+            projectGoalMap.set(item,{'amount':goalAmounts[index], 'compelete':false});
         });
 
         donationProjectInfo.projecttID = args[0];
@@ -233,54 +233,23 @@ var Chaincode = class {
         let projectUpdated = await stub.getState(searchProjectKey);
         let projectInfo = JSON.parse(projectUpdated);
 
-        //donation key 생성
-        let compositeDonationKey = stub.createCompositeKey('Donation.Account', [args[0], args[1]]);
-
-
         if (projectInfo.state !== 'ProjectRegistered') {
             throw new Error('Unavailable project: Project has not been registered.');
         } else if (projectInfo.state === 'ProjectDeleted') {
             throw new Error('Unavailable project: Project has already been deleted.');
         } else {
-
-            console.log(`participatedDonationProjects :  ${userInfo.participatedDonationProjects}`);
-            console.log(`participatedDonationProjects.length : ${userInfo.participatedDonationProjects.length}`);
-
-            ///여기 부분 아래를 싹 뜯어 고처야한다. 
-            let updateDonation = await stub.getState(compositeDonationKey);
-            //get을 통해 가져와서 등록전에 똑같은애가 있는지를 확인한다.
-            let isExist = function (value) {
-                if (value == "" || value == null || value == undefined ||
-                    (value != null && typeof value == "object" && !Object.keys(value).length)) {
-                    return true;
-                } else {
-                    //값이 들어있으면 여기
-                    return false;
-                }
-            };
-            let donationInfo = {
-                amount : 0,
-                state : '',
-                createdAt : '',
-            }
-            if (isExist(updateDonation) != true) {
-                //값이 있으면 업데이트 처리해주어야한다.
-                //키값에 저장된 데이터만 업데이트 처리해 주면된다.
-                let stateDonationInfo = JSON.parse(updateDonation);
-                donationInfo.amount = stateDonationInfo.amount;
-                donationInfo.state = stateDonationInfo.state;
-
-            }else{   
-                userInfo.participatedDonationProjects.push(args[1]);   
+            
+            console.log(`participatedDanationProjects :  ${userInfo.participatedDanationProjects}`);
+            console.log(`participatedDanationProjects.length : ${userInfo.participatedDanationProjects.length}`);
+            let isParticipated = userInfo.participatedDanationProjects.findIndex((item, index)=>{return item === args[1] }) >= 0;
+            
+            if(isParticipated===false){
+                userInfo.participatedDanationProjects.push(args[1]);
                 projectInfo.participants.push(args[0]);
             }
-            donationInfo.amount = parseInt(donationInfo.amount) + parseInt(args[2]);
-            donationInfo.state = 'donated';
-            
-
-            userInfo.totalDonationAmount = parseInt(userInfo.totalDonationAmount) + parseInt(args[2]);
+            userInfo.totalDonationAmount =  parseInt(userInfo.totalDonationAmount) + parseInt(args[2]);
             projectInfo.currentAmount = parseInt(projectInfo.currentAmount) + parseInt(args[2]);
-
+            
             let txTimestamp = stub.getTxTimestamp();
             let tsSec = txTimestamp.seconds;
             let tsSecValue = tsSec.low + (540 * 60);
@@ -289,11 +258,9 @@ var Chaincode = class {
 
             projectInfo.createdAt = timestampString;
             userInfo.createdAt = timestampString;
-            donationInfo.createdAt = timestampString;
 
             await stub.putState(searchUserKey, Buffer.from(JSON.stringify(userInfo)));
             await stub.putState(searchProjectKey, Buffer.from(JSON.stringify(projectInfo)));
-            await stub.putState(compositeDonationKey, Buffer.from(JSON.stringify(donationInfo)));
         }
     }//donateToProject-end
 
@@ -303,95 +270,15 @@ var Chaincode = class {
      * @param {string[]} args 
      * [projecttID, organizer, goalID, contents ]
      */
-    async compeleteGoalsDonationProject(stub, args) {
-        if (args.length !== 4) {
+    compeleteGoalsDonationProject(stub, args){
+        if(args.length!==4){
             throw new Error('Incorrect Number of arguments Expecting 6, but received input args.length : ' + args.length);
         }
-        let searchProjectKey = stub.createCompositeKey('DonationProject.ID.', [args[0]]);
-        let projectUpdated = await stub.getState(searchProjectKey);
-        let projectInfo = JSON.parse(projectUpdated);
+        let searchKey = stub.createCompositeKey('DonationProject.ID.', [args[0]]);//projectID를 넣고 찾는다.
 
-        if (projectInfo.organizer !== args[1]) {
-            throw new Error(`Incorrect organizer of project! ${args[1]} is not organizer this project `);
-        }
-        if (!(projectInfo.projectGoalMap.has(args[2]))) {
-            throw new Error(`project doesn't have goal which name is ${args[2]}`);
-        }
-        if (projectInfo.projectGoalMap.get(args[2]).compelete) {
-            throw new Error(`${args[2]} is already compelete`);
-        }
-        if (projectInfo.state !== 'ProjectRegistered') {
-            throw new Error('Unavailable project: Project has not been registered.');
-        } else if (projectInfo.state === 'ProjectDeleted') {
-            throw new Error('Unavailable project: Project has already been deleted.');
-        } else {
-            projectInfo.projectGoalMap.set(args[2], { 'amount': projectInfo.projectGoalMap.get(args[2]).amount, 'compelete': true, 'contents': args[3] });
-            await stub.putState(searchProjectKey, Buffer.from(JSON.stringify(projectInfo)));
-        }
     }
 
-    /**
-     * -6- donationPJT 종료
-     * @param {Object} stub 
-     * @param {string[]} args 
-     * [projecttID, userID]
-     */
-    async endDonationProject(stub, args) {
-        if (args.length !== 2) {
-            throw new Error('Incorrect Number of arguments Expecting 2, but received input args.length : ' + args.length);
-        }
-        let searchProjectKey = stub.createCompositeKey('DonationProject.ID.', [args[0]]);
-        let projectUpdated = await stub.getState(searchProjectKey);
-        let projectInfo = JSON.parse(projectUpdated);
-
-        if (projectInfo.organizer !== args[1]) {
-            throw new Error(`${args[1]} is not organizer about ${args[0]}`);
-        }
-        for (let [key, value] of projectInfo.projectGoalMap) {
-            if (!value.compelete) {
-                throw new Error(`${args[0]} doesn't conelete all goals`);
-            }
-        }
-        if (projectInfo.state !== 'ProjectRegistered') {
-            throw new Error('Unavailable project: Project has not been registered.');
-        } else if (projectInfo.state === 'ProjectDeleted') {
-            throw new Error('Unavailable project: Project has already been deleted.');
-        } else {
-            projectInfo.state = 'ProjectEnd';
-            await stub.putState(searchProjectKey, Buffer.from(JSON.stringify(projectInfo)));
-        }
-    }
-
-    /**
-     * -7- 환불 신청 
-     * @param {Object} stub 
-     * @param {string[]} args 
-     * [userID, projectID]
-     */
-    async requestRefund(stub, args) {
-        if (args.length !== 2) {
-            throw new Error('Incorrect Number of arguments Expecting 2, but received input args.length : ' + args.length);
-        }
-        //유저 찾기
-        //UserID, projectID를 기반으로 dontion.Account찾아서 바꾸기 다른 부분 바꿀 필요없이
-        
-            
-    }
-
-    /**
-     * 
-     * @param {Object} stub 
-     * @param {string[]} args 
-     * [userID, refundAmount]
-     */
-    async compeleteRefund(stub, args) {
-        if (args.length !== 2) {
-            throw new Error('Incorrect Number of arguments Expecting 2, but received input args.length : ' + args.length);
-        }
-        // let searchUserKey = stub.createCompositeKey('User.Account.', )
-    }
-
-    //-==============================QUERY=============================
+//-==============================QUERY=============================
     /**
      * -query-
      * @param {Object} stub 
@@ -399,23 +286,23 @@ var Chaincode = class {
      * @return {string}
      * <userID, projectID...> length 1
      */
-    async query(stub, args) {
+    async query(stub, args){
         let searchKey = '';
         if (args.length !== 2) {
             throw new Error('query :  Incorrect Number of arguments Expecting 3, but received input args.length : ' + args.length);
         }
-        if (args[0] === 'searchUser') {
-            searchKey = stub.createCompositeKey('User.Account.', [args[0]]);
-
-        } else if (args[0] === 'searchDonationProject') {
+        if(args[0] === 'searchUser'){
+            searchKey = stub.createCompositeKey('User.Account.',[args[0]]);
+            
+        }else if(args[0] === 'searchDonationProject'){
             //DonationProject.ID.
-            searchKey = stub.createCompositeKey('User.Account.', [args[0]]);
-        } else {
+            searchKey = stub.createCompositeKey('User.Account.',[args[0]]);
+        }else{
             throw new Error(`query don't have ${arg[0]}`);
         }
         let value = await stub.getState(searchKey);
-        console.log(value);
-
+            console.log(value);
+        
         return value;
     }
 
